8ba8c2e09688a206abe6d25284a3cf4b358eeac0
Yoga Prastyo <yogaprastyobayu@gmail.com>
2026-05-17 06:39:23 +0700

    fix: ganti --listen ke -l agar kompatibel dengan serve v14

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "talenthub-landing",
       script: "node_modules/.bin/serve",
-      args: "dist --single --listen 8080",
+      args: "dist --single -l 8080",
       interpreter: "none",
       env: {
         NODE_ENV: "production",
